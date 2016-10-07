7e7b20df5ac119a52c675c3fdce7acae56f931b7
Brendan Regnery <bregnery@ufl.edu>
2016-10-07 18:52:08 +0200

    Merging

--- UFDiMuonsAnalyzer/python/UFDiMuonsAnalyzer_MC_cff.py
+++ UFDiMuonsAnalyzer/python/UFDiMuonsAnalyzer_MC_cff.py
@@ -6,7 +6,7 @@ DiMuons = cms.EDAnalyzer('UFDiMuonsAnalyzer',
                          muonColl = cms.InputTag("slimmedMuons"),
                          
                          isVerbose = cms.untracked.bool(False),
-                         isMonteCarlo = cms.bool(False),
+                         isMonteCarlo = cms.bool(True),
                          nMuons = cms.int32(2),
                          
                          # muon kinematic cuts
@@ -15,42 +15,23 @@ DiMuons = cms.EDAnalyzer('UFDiMuonsAnalyzer',
                          isTracker   =cms.int32(1),
                          ptMin       = cms.double(10),
                          etaMax      = cms.double(2.4),
-                         normChiSquareMax= cms.double(999),
-
-                         # number of hits cuts
-                         minMuonHits	  = cms.int32(-999),
-                         minPixelHits	  = cms.int32(-999),
-                         minStripHits	  = cms.int32(-999),
-                         minTrackerHits	  = cms.int32(-999),
-                         minSegmentMatches= cms.int32(-999),
-                         minNumOfMatchedStations = cms.int32(-999),
-
-                         minPixelLayers  = cms.int32(-999),
-                         minTrackerLayers= cms.int32(-999),
-                         minStripLayers  = cms.int32(-999),
-                         validFracTrackerMin= cms.int32(-999),   
     
                          primaryVertexTag = cms.InputTag("offlineSlimmedPrimaryVertices"),
                          beamSpotTag = cms.InputTag("offlineBeamSpot"),
                          prunedGenParticleTag = cms.InputTag("prunedGenParticles"),
                          packedGenParticleTag = cms.InputTag("packedGenParticles"),
-                         d0Max = cms.double(999), 
-                         
-                         #track isolation
-                         trackIsoMaxSumPt = cms.double(999),
-                         relCombIsoMax    = cms.double(999),
                          
                          #triggerName = cms.string("@"),#wild card: all triggers
                          # HLT trigger info
-                         checkTrigger = cms.bool(True),
+                         checkTrigger = cms.bool(False),
 
                          processName = cms.string("HLT"),
-                         triggerNames = cms.vstring("HLT_IsoMu20","HLT_IsoTkMu20","HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ"), # up to 3 trigger names
+                         triggerNames = cms.vstring("HLT_IsoMu20", "HLT_IsoTkMu20","HLT_IsoMu22", "HLT_IsoTkMu22","HLT_IsoMu24","HLT_IsoTkMu24"), # up to 6 trigger names
+
                          triggerResults = cms.InputTag("TriggerResults","","HLT"),
                          triggerObjs = cms.InputTag("selectedPatTrigger"),
                          
                          metTag         = cms.InputTag("slimmedMETs"),
                          pfJetsTag      = cms.InputTag("slimmedJets"),
                          genJetsTag     = cms.InputTag("slimmedGenJets")
-                         
                          )
